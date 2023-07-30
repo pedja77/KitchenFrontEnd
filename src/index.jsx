665ee4612b318eb3978af48ce1beb470daf486ec
pedja77 <pedja77@stcable.net>
2023-07-30 16:03:13 +0200

    zaostala izmena u index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const BASE_URI = "httl://localhost:8080/"
+
+const router = createBrowserRouter([
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
