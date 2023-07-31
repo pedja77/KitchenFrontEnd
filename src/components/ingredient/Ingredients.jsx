92670ed6ae56a3dce5f96997655fd93b82096ab1
pedja77 <pedja77@stcable.net>
2023-07-31 22:27:35 +0200

    Dodata TableTemplate komponenta

--- src/components/ingredient/Ingredients.jsx
+++ src/components/ingredient/Ingredients.jsx
@@ -1,7 +1,22 @@
 import { Typography } from "@mui/material"
+import TableTemplate from "../lib/TableTemplate";
 
+
+const ingredientsTableProps = {
+    tableLabel: "Ingredients",
+    tableHeaders: ["Name", "Unit", "Calories(kcal)", "Carbohydrates", "Sugars", "Fats", "Saturated fats", "Proteins"],
+    tableData: null,
+    tdConfig: ["id", "firstName", "lastName"],
+    removeFn: undefined,
+    collectionName: "undefined",
+    editUrl: "/",
+  };
 const Ingredients = () => {
-    return <Typography>Ingredients list</Typography>
-}
+    return (
+        <>
+        <Typography>Ingredients list</Typography>
+        <TableTemplate props={ingredientsTableProps} />
+        </>
+)}
 
 export default Ingredients;
