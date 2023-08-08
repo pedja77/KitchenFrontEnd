c369a0d109975c0bb739e9829fe16656d2e97198
Nikola Kovacevic <nikolakovacevic.ns.2@gmail.com>
2023-08-08 14:22:40 +0200

    home page malo doteran, pocet rad na prikazu individualnih recepta

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,25 +3,33 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import RecipeCard from "../lib/RecipeCard";
 
+
+
+
+
+
 const Home = () => {
     const recipes = useLoaderData();
     const [recipesList, setRecipes] = useState(structuredClone(recipes));
    
-   
-   
-   
+
    
    
     return (
     <>
     {/* <Typography sx={{textAlign:"center", paddingTop:"3px"}}>Welcome to Hell's Kitchen!!</Typography> */}
-    <Container sx={{backgroundColor: "#bf360c"}}>
+    <Container sx={{backgroundColor: "#bf360c", padding:"30px 30px"}}>
     <Grid container spacing={3}>
         {recipesList.map((r) => (
           <RecipeCard props={r}  />
         ))}
       </Grid>
+      
     </Container>
+    <Container>
+        <Typography>Some info</Typography>
+      </Container>
+    
     </>
     )     
 }
--- src/components/lib/RecipeCard.jsx
+++ src/components/lib/RecipeCard.jsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardHeader, CardMedia, Grid, List } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Grid, List } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 
@@ -8,8 +9,19 @@ const RecipeCard = ({props}) => {
 
     return (
         <>
-        <Grid item xs={4}>
-            <Card   >
+        <Grid item xs={4}  >
+            <Card sx={
+                    {"&:hover": {
+                        boxShadow: "10",
+                        opacity: "70%",
+                        color: "white",
+                        cursor: "pointer"
+                }
+           }}  onClick={() => nav(`/recipe/${props.id}`)}>
+             <CardHeader sx={{display: "flex", textAlign: "center", 
+                backgroundColor: "rgb(224, 94, 43)", paddingBottom:"2"}} 
+                title={props.title} subheader={`Created on:  ${props.createdOn}`}/>
+            
             <CardMedia
                  sx={{justifyContent:"center"}}  
                  component="svg"
@@ -19,19 +31,17 @@ const RecipeCard = ({props}) => {
                 image="https://upload.wikimedia.org/wikipedia/commons/2/2e/Food-luxury-pasta.svg"
                 title={props.title}
                 />
-                <CardHeader sx={{display: "flex", textAlign: "center", 
-                backgroundColor: "rgb(224, 94, 43)", paddingBottom:"2"}} 
-                title={props.title} subheader={`Created on:  ${props.createdOn}`}/>
-                  
                 <CardContent sx={{display: "flex", flexDirection: "column", alignItems:"center", backgroundColor: "rgb(224, 94, 43)"}}>
                 {props.description} 
                 <br/>
-                {props.timeToPrepare} minutes
-                <br/>
                 By: {props.cook}
                 <br/>
-                
+                {/* {props.ingredients.map( e=> e.name)}  */}
+                <br/>
+                {/*test iteracije kroz mapu a i podsetnik za NK*/}
+                {/* {Object.entries(props.ingredientAmount).map(([key, value]) =>  `${key}: ${value}`)}  */}
                 </CardContent>
+               
                 
             </Card>
         </Grid>
